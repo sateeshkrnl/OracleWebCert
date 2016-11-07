61dafe9d65616a32ad078c12509a13bf39ece4ac
chinnu <chinnu@192.168.1.101>
2016-11-08 01:02:06 +0530

    commit

--- TestWebProj/src/com/test/bean/Hello.java
+++ TestWebProj/src/com/test/bean/Hello.java
@@ -1,13 +1,24 @@
 package com.test.bean;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 
 @ManagedBean
+@RequestScoped
 public class Hello {
 
 	private String world = "Hello World!";
+	private String name = "";
 	
 	public String getWorld(){
 		return world;
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public String getName(){
+		return name;
 	}
 }
