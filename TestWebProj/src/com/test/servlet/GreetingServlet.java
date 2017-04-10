7940d9ec467beb1aec677726093f78517fdab7b4
Sateesh Kumar V <sateeshkrnl@gmail.com>
2017-04-10 15:40:00 +0530

    commit

--- TestWebProj/src/com/test/servlet/GreetingServlet.java
+++ TestWebProj/src/com/test/servlet/GreetingServlet.java
@@ -3,6 +3,7 @@ package com.test.servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.inject.Inject;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,12 +11,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.test.bean.ITestRefBean;
+import com.test.bean.TestRefImpl2;
+
 @WebServlet("/greeting")
 public class GreetingServlet extends HttpServlet {
+	@Inject @TestRefImpl2 ITestRefBean testRefBean;
 	
 	@Override
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
+		System.out.println(testRefBean.getTestRefValue());
 		resp.setContentType("text/html");
 		resp.setBufferSize(8192);
 		PrintWriter out = resp.getWriter();
